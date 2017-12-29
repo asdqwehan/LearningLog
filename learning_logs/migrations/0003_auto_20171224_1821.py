7aad156de2940801b91e7d22119f4c36d9e14f3d
ehmatthes <eruc@example.com>
2017-12-29 14:21:12 +0800

    Ready for deployment to heroku.

---
+++ learning_logs/migrations/0003_auto_20171224_1821.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('learning_logs', '0002_entry'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='topic',
+            old_name='data_added',
+            new_name='date_added',
+        ),
+    ]
